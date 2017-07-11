8a295a52871c9ec607befd02da69406d680283ae
Josh Gendal <joshuagendal@Joshuas-MBP.domain>
2017-07-11 11:37:40 -0600

    Added event controllers and event post validation

---
+++ controllers/eventController.js
@@ -0,0 +1,53 @@
+const Event = require('../models/event');
+
+let getAllEvents = (req, res) => {
+    Event.find({}, function(err, allEvents){
+        if(err){
+            console.log(err);
+            res.redirect('/');
+        } else {
+            res.render('events/show-all.ejs', {events: allEvents});
+        }
+    });
+}	
+
+let getNewEventPage = (req, res) => {
+    res.render('events/new-event.ejs')
+};
+
+let postNewEvent = (req, res) => {
+    const eventTitle = req.body.eventTitle;
+    const eventDescription = req.body.eventDescription;
+    const userThatAddedEvent = {
+        id: req.user._id,
+        username: req.user.username
+    }
+    const created = new Date();
+    const newEvent = {eventTitle: eventTitle, eventDescription: eventDescription, userThatAddedEvent: userThatAddedEvent, created: created}	
+    Event.create(newEvent, function(err, newlyAddedEvent){
+        if(err){
+            console.log(err);
+            res.redirect('/');
+        } else {
+            res.redirect('/events');
+        }
+    });
+};
+
+let eventPostValidation = (req, res, next) => {
+    if(!req.body.eventTitle || !req.body.eventDescription) {
+        req.flash('error', 'Your event must have a title and a decription! Please try again');
+        res.redirect('/events/new-event');
+    } else {
+        next();
+    }
+}
+
+
+
+module.exports = {
+    getAllEvents,
+    getNewEventPage,
+    postNewEvent,
+    eventPostValidation
+}
--- routes/event.js
+++ routes/event.js
@@ -1,39 +1,41 @@
-var Event = require('../models/event');
-var middleware = require('../middleware/functions.js');
+const Event = require('../models/event');
+const actions = require('../controllers/eventController.js');
+const middleware = require('../middleware/functions.js');
+
 
 module.exports = (app) => {
-	app.get('/events', (req, res) => {
-		Event.find({}, function(err, allEvents){
-			if(err){
-				console.log(err);
-				res.redirect('/');
-			} else {
-				res.render('events/show-all.ejs', {events: allEvents});
-			}
-		});
-	});	
+
+	// 	GET ALL EVENTS
+	app.get('/events', actions.getAllEvents);
 	
-	app.get('/events/new-event', middleware.isUserLoggedIn, (req, res) => {
-		res.render('events/new-event.ejs')
-	});
+	// GET NEW EVENT PAGE
+	app.get('/events/new-event', middleware.isUserLoggedIn, actions.getNewEventPage);
+	
+	// (req, res) => {
+	// 	res.render('events/new-event.ejs')
+	// });
 
-	app.post('/events/new-event', middleware.isUserLoggedIn, (req, res) => {
-		var eventTitle = req.body.eventTitle;
-		var eventDescription = req.body.eventDescription;
-		var userThatAddedEvent = {
-			id: req.user._id,
-			username: req.user.username
-		}
-		var created = new Date();
-		var newEvent = {eventTitle: eventTitle, eventDescription: eventDescription, userThatAddedEvent: userThatAddedEvent, created: created}	
-		Event.create(newEvent, function(err, newlyAddedEvent){
-			if(err){
-				console.log(err);
-				res.redirect('/');
-			} else {
-				res.redirect('/events');
-			}
-		});
-	});
+	// POST EVENT
+	app.post('/events/new-event', middleware.isUserLoggedIn, actions.eventPostValidation, actions.postNewEvent);
+
 }
-
+	
+// 	(req, res) => {
+// 		var eventTitle = req.body.eventTitle;
+// 		var eventDescription = req.body.eventDescription;
+// 		var userThatAddedEvent = {
+// 			id: req.user._id,
+// 			username: req.user.username
+// 		}
+// 		var created = new Date();
+// 		var newEvent = {eventTitle: eventTitle, eventDescription: eventDescription, userThatAddedEvent: userThatAddedEvent, created: created}	
+// 		Event.create(newEvent, function(err, newlyAddedEvent){
+// 			if(err){
+// 				console.log(err);
+// 				res.redirect('/');
+// 			} else {
+// 				res.redirect('/events');
+// 			}
+// 		});
+// 	});
+// }

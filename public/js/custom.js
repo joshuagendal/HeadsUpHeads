9ff8276501abf205f6c5e919713206a94ab7962e
Josh Gendal <joshuagendal@Joshuas-MBP.domain>
2017-07-10 22:26:25 -0600

    fixed some front end issues

---
+++ public/js/custom.js
@@ -0,0 +1,24 @@
+// JavaScript source code
+
+$("document").ready(readyFunction);
+
+function readyFunction() {
+    $(".scroll-btn").on("click", scrollToContent);
+};
+
+function scrollToContent() {
+    $("body, html").animate({ scrollTop: $(".content").position().top }, 500);
+}
+
+function changeCollapsedBackground() {
+    $("#navbar").attr("aria-expanded", "true").css("background-image", "url(../img/threepnto.png)"); 
+}
+
+// Navbar Collapse and Background
+
+$(window).resize(function(){
+    if ($(this).width() > 767) {
+        $('#navbar').removeClass('in');
+    };
+});
+

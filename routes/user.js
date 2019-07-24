39571f273a69d3c475c71520123b399195382ffe
Joshua Gendal <joshgendal@gmail.com>
2019-07-23 23:33:01 -0600

    gets to pwd reset pg

--- routes/user.js
+++ routes/user.js
@@ -1,35 +1,46 @@
-var actions = require('../controllers/userController');
-var passport = require('passport');
+var actions = require("../controllers/userController");
+var passport = require("passport");
 
-module.exports = (app) => {
-	
-	// GET SIGNUP FORM
-  app.get('/signup', actions.getSignupForm);
+module.exports = app => {
+  // GET SIGNUP FORM
+  app.get("/signup", actions.getSignupForm);
 
-	// POST SIGNUP FORM
-	// STEPS: 1. Client-side validation           
-	// 2. Server-side validation
-	app.post('/signup', actions.signUpValidation, passport.authenticate('local.signup', {
-		successRedirect: '/',
-		failureRedirect: '/signup',
-		failureFlash: true
-	}));
+  // POST SIGNUP FORM
+  // STEPS: 1. Client-side validation
+  // 2. Server-side validation
+  app.post(
+    "/signup",
+    actions.signUpValidation,
+    passport.authenticate("local.signup", {
+      successRedirect: "/",
+      failureRedirect: "/signup",
+      failureFlash: true
+    })
+  );
 
-	// GET LOGIN FORM
-  app.get('/login', actions.getLoginForm);
+  // GET LOGIN FORM
+  app.get("/login", actions.getLoginForm);
 
-	// POST LOGIN FORM
-	// STEPS: 1. Client-side validation
-	// 2. Server-side validation
-  app.post('/login', actions.loginValidation, passport.authenticate('local.login', { // @TODO add in function to make sure user is verified by admin
-		successRedirect: '/', 
-		failureRedirect: '/login',
-		failureFlash : true
-	}));
+  // POST LOGIN FORM
+  // STEPS: 1. Client-side validation
+  // 2. Server-side validation
+  app.post(
+    "/login",
+    actions.loginValidation,
+    passport.authenticate("local.login", {
+      // @TODO add in function to make sure user is verified by admin
+      successRedirect: "/",
+      failureRedirect: "/login",
+      failureFlash: true
+    })
+  );
 
-	// LOGOUT
-	app.get('/logout', actions.logout);
+  // LOGOUT
+  app.get("/logout", actions.logout);
 
-	// DELETE USER
-	app.delete('/:id/1m0a7c53ndtkejd', actions.deleteUser);
-}    
+  // PASSWORD RESET
+  app.get("/password-reset", actions.getPasswordResetForm);
+
+  // DELETE USER
+  app.delete("/:id/1m0a7c53ndtkejd", actions.deleteUser);
+};
